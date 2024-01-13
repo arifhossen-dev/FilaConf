f657c70dbdfc884a1d61b6bf78998999be9c82dd
arifhossen-dev <ahak.bsl@gmail.com>
2024-01-13 23:07:19 +0600

    Attendee belongsTo conference Model relation create

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Attendee extends Model
 {
     use HasFactory;
+
+    public function conference(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Conference::class);
+    }
 }
